9e53cb03fd763bb1ee5d798f36ee20049467d8a3
Andreas <andreas221b@gmail.com>
2023-11-26 20:33:22 +0100

    Add julia set

--- QtFractals/widget.cpp
+++ QtFractals/widget.cpp
@@ -1,5 +1,7 @@
 #include "widget.h"
 
+#include <QComboBox>
+#include <QDebug>
 #include <QDoubleSpinBox>
 #include <QHBoxLayout>
 #include <QPushButton>
@@ -10,11 +12,13 @@ Widget::Widget(QWidget *parent) : QWidget{parent} {
   this->setLayout(m_grid);
 }
 
-void Widget::updateImageData() {
+void Widget::CreateMdlbrotSet() {
   auto maxColor = 255;
   auto maxIter = 55;
+
   QImage im(QSize(m_imageWidth, m_imageHeight), QImage::Format_ARGB32);
   QColor pixel;
+
   for (int i = 0; i < m_imageWidth; ++i) {
     for (int j = 0; j < m_imageHeight; ++j) {
       std::complex<double> C(
@@ -40,11 +44,56 @@ void Widget::updateImageData() {
   m_imageLabel->setPixmap(QPixmap::fromImage(im));
 }
 
-void Widget::genImageButtonClicked() { updateImageData(); }
+void Widget::CreateJuliaSet() {
+  auto maxColor = 255;
+  auto maxIter = 55;
+  std::complex<double> C(m_realNumber, m_imgNumber);
+
+  QImage im(QSize(m_imageWidth, m_imageHeight), QImage::Format_ARGB32);
+  QColor pixel;
+
+  for (int i = 0; i < m_imageWidth; ++i) {
+    for (int j = 0; j < m_imageHeight; ++j) {
+      std::complex<double> Z(static_cast<double>(i) / m_imageWidth - 0.5,
+                             static_cast<double>(j) / m_imageHeight - 0.5);
+      int iterations = 0;
+
+      while (abs(Z) < 2.0 && iterations < maxIter) {
+        Z = Z * Z + C;
+        iterations++;
+      }
+
+      if (iterations < maxIter) {
+        int color = int(maxColor * iterations / maxIter);
+        pixel = QColor::fromRgb(color, color, 0);
+      } else {
+        pixel = QColor::fromRgb(0, 0, 0);
+      }
+      im.setPixelColor(i, j, pixel);
+    }
+  }
+  m_imageLabel->setPixmap(QPixmap::fromImage(im));
+}
+
+void Widget::genImageButtonClicked() {
+  if (m_set == Set::Mandelbrot) {
+    CreateMdlbrotSet();
+  } else if (m_set == Set::Julia) {
+    CreateJuliaSet();
+  }
+}
 
 void Widget::realSpinboxValueChanged(double d) { m_realNumber = d; }
 
 void Widget::imgSpinboxValueChanged(double d) { m_imgNumber = d; }
+
+void Widget::setComboBoxChanged(int index) {
+  if (index == 0) {
+    m_set = Set::Mandelbrot;
+  } else if (index == 1) {
+    m_set = Set::Julia;
+  }
+}
 
 void Widget::setupGrid() {
   m_grid = new QGridLayout(this);
@@ -76,9 +125,17 @@ void Widget::setupGrid() {
   QObject::connect(genButton, &QPushButton::clicked, this,
                    &Widget::genImageButtonClicked);
 
+  QComboBox *cbox = new QComboBox(this);
+  cbox->addItem("Mandelbrot Set");
+  cbox->addItem("Julia Set");
+  cbox->setCurrentIndex(0);
+  QObject::connect(cbox, &QComboBox::activated, this,
+                   &Widget::setComboBoxChanged);
+
   m_grid->addWidget(m_imageLabel, 0, 0, 1, 2);
   m_grid->addItem(realBox, 1, 0, 1, 1);
   m_grid->addItem(imgBox, 2, 0, 1, 1);
-  m_grid->addWidget(genButton, 1, 1, 2, 1);
+  m_grid->addWidget(genButton, 1, 1, 1, 1);
+  m_grid->addWidget(cbox, 2, 1, 2, 1);
   m_grid->setAlignment(Qt::AlignCenter);
 }
--- QtFractals/widget.h
+++ QtFractals/widget.h
@@ -10,20 +10,25 @@ class Widget : public QWidget {
  public:
   explicit Widget(QWidget *parent = nullptr);
 
-  void updateImageData();
+  void CreateMdlbrotSet();
+  void CreateJuliaSet();
 
  private slots:
   void genImageButtonClicked();
   void realSpinboxValueChanged(double d);
   void imgSpinboxValueChanged(double d);
+  void setComboBoxChanged(int index);
 
  private:
+  enum class Set { Mandelbrot, Julia };
+
   QGridLayout *m_grid;
   QLabel *m_imageLabel;
-  int m_imageWidth = 500;
-  int m_imageHeight = 500;
+  int m_imageWidth = 800;
+  int m_imageHeight = 800;
   double m_realNumber = 0.;
   double m_imgNumber = 0.;
+  Set m_set;
 
   void setupGrid();
 };
