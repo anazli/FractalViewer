6e033e2d52cb1961eded40406197544892e8eb9f
Andreas <andreas221b@gmail.com>
2023-11-26 20:52:52 +0100

    Update parameters

--- QtFractals/widget.cpp
+++ QtFractals/widget.cpp
@@ -21,10 +21,9 @@ void Widget::CreateMdlbrotSet() {
 
   for (int i = 0; i < m_imageWidth; ++i) {
     for (int j = 0; j < m_imageHeight; ++j) {
-      std::complex<double> C(
-          static_cast<double>(i) / m_imageWidth - m_realNumber,
-          static_cast<double>(j) / m_imageHeight - m_imgNumber);
-      std::complex<double> Z(0.f, 0.f);
+      std::complex<double> C(static_cast<double>(i) / m_imageWidth - 1.5,
+                             static_cast<double>(j) / m_imageHeight - 0.5);
+      std::complex<double> Z(m_realNumber, m_imgNumber);
 
       int iterations = 0;
       while (abs(Z) < 2.0 && iterations < maxIter) {
@@ -46,7 +45,7 @@ void Widget::CreateMdlbrotSet() {
 
 void Widget::CreateJuliaSet() {
   auto maxColor = 255;
-  auto maxIter = 55;
+  auto maxIter = 35;
   std::complex<double> C(m_realNumber, m_imgNumber);
 
   QImage im(QSize(m_imageWidth, m_imageHeight), QImage::Format_ARGB32);
@@ -105,6 +104,8 @@ void Widget::setupGrid() {
   QLabel *realLabel = new QLabel(this);
   realLabel->setText(tr("Real Number:"));
   QDoubleSpinBox *realNumber = new QDoubleSpinBox(this);
+  realNumber->setRange(-10., 10.);
+  realNumber->setDecimals(3);
   realBox->addWidget(realLabel);
   realBox->addWidget(realNumber);
 
@@ -115,6 +116,8 @@ void Widget::setupGrid() {
   QLabel *imgLabel = new QLabel(this);
   imgLabel->setText(tr("Img. Number:"));
   QDoubleSpinBox *imgNumber = new QDoubleSpinBox(this);
+  imgNumber->setRange(-10., 10.);
+  imgNumber->setDecimals(3);
   imgBox->addWidget(imgLabel);
   imgBox->addWidget(imgNumber);
 
@@ -128,9 +131,9 @@ void Widget::setupGrid() {
   QComboBox *cbox = new QComboBox(this);
   cbox->addItem("Mandelbrot Set");
   cbox->addItem("Julia Set");
-  cbox->setCurrentIndex(0);
   QObject::connect(cbox, &QComboBox::activated, this,
                    &Widget::setComboBoxChanged);
+  cbox->setCurrentIndex(0);
 
   m_grid->addWidget(m_imageLabel, 0, 0, 1, 2);
   m_grid->addItem(realBox, 1, 0, 1, 1);
